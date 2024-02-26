4ec7e953420dc6519d380a04ae53d51a1d953e47
Dheeraj <dheeru2605@gmail.com>
2024-02-25 21:43:10 -0800

    Added computer and human player classes

--- player.py
+++ player.py
@@ -5,3 +5,18 @@ import math
 class Player:
     def __init__(self, letter): # letter is either X or O
         self.letter = letter
+
+    def move(self, game):
+        pass
+
+
+class ComputerPlayer(Player):  # Computer class
+    def __init__(self, letter):
+        super().__init__(letter)   # use initial constructor in player class 
+
+    def move(self, game):
+        pass
+        
+class HumanPlayer(Player):  # Human class
+    def __init__(self, letter):
+        super().__init__(letter)
