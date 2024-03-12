2a8ef223a1dcc0fbd54a532142c566a8fff004ab
Dheeraj <dheeru2605@gmail.com>
2024-03-11 21:50:33 -0700

    added functionality to HumanPlayer class

--- player.py
+++ player.py
@@ -15,11 +15,22 @@ class ComputerPlayer(Player):  # Computer class
         super().__init__(letter)   # use initial constructor in player class 
 
     def move(self, game):
-        pass
+        emptySquare = random.choice(game.availableMoves()) # choose a random empty square on the board 
+        return emptySquare
         
 class HumanPlayer(Player):  # Human class
     def __init__(self, letter):
         super().__init__(letter)
 
     def move(self, game):
-        pass
+        validSquare = False
+        value = None
+        while not validSquare:
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            try:
+                value = int(square)
+                if value not in game.availableMoves():
+                    raise ValueError
+                validSquare = True
+            except ValueError:
+                print('Invalid square. Try again!')
